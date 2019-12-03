3d4c919da5c1060b0fed76bc154869078b0ddb01
Bob YC Li/WSH/Wistron <Bob_YC_Li@wistron.com>
2019-12-03 17:28:45 +0800

    fat12 coding

--- chapter3/boot.asm
+++ chapter3/boot.asm
@@ -1,6 +1,28 @@
 org 0x7c00
 
 BaseOfStack equ 0x7c00
+BaseOfLoader equ 0x1000
+OffsetOfLoader equ 0x00
+
+RootDirSectors equ 14
+SectorNumOfRootDirStart equ 19
+SectorNumOfFAT1Start equ 1
+SectorBalance equ 17
+
+jmp short Label_Start
+nop
+BS_OEMName db 'MINEboot'
+BPB_BytesPerSec dw 512
+BPB_SecPerClus db 1
+BPB_RsvdSecCnt dw 1
+BPB_NumFATs db 2
+BPB_RootEntCnt dw 224
+BPB_TotSec16 dw 2880
+BPB_Media db 0xf0
+BPB_FATSz16 dw 9
+BPB_SecPerTrk dw 18
+BPB_NumHeads dw 2
+
 
 Label_Start:
 
